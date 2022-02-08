84b6493c7a12d2f339bd7ac3bd0b869167671384
IvlevaAnna <annaiva19@mail.ru>
2022-02-08 21:13:06 +0300

    [Test] Cards and popup created

--- src/src/App.js
+++ src/src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Main} from "./views/Main/Main";
+import {Main} from "./views/Main";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-          <Routes>
-              <Route path='' element={<Main/>} />
-          </Routes>
-      </BrowserRouter>
-
+      <Main/>
     </div>
   );
 }
